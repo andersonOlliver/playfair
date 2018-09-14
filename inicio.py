9d15ca76a1d6f205d248dde05703930afa6c378a
Olliver <Olliver@DESKTOP-BO31HLM>
2018-09-14 03:17:11 -0300

    Adiciona criptografia

--- inicio.py
+++ inicio.py
@@ -9,16 +9,6 @@ def cls():
     os.system(['clear', 'cls'][os.name == 'nt'])
 
 
-def indice_do_elemento(matriz, elemento):
-    posicao = 0
-    for linha in matriz:
-        for coluna in linha:
-            if coluna == elemento:
-                return [posicao / 5, posicao % 5]
-
-            posicao += 1
-
-
 #################################################################
 #                          ARQUIVO                              #
 #################################################################
@@ -58,6 +48,7 @@ def ler_arquivo_saida():
 def gravar_arquivo(caminho, conteudo):
     f = open(caminho, "w+")
     f.write(conteudo)
+    f.close()
 
 
 #################################################################
@@ -85,16 +76,17 @@ def gerar_matriz_chave(chave: str):
     posicao: int = 0
 
     for letra in chave:
-        linha = int(posicao / 5)
-        coluna = int(posicao % 5)
+        if (letra >= "A") and (letra <= "Z"):
+            linha = int(posicao / 5)
+            coluna = int(posicao % 5)
 
-        if letra.upper() == "Y":
-            letra = "X"
+            if letra.upper() == "Y":
+                letra = "X"
 
-        if letra in alfabeto:
-            alfabeto.remove(letra)
-            matriz[linha][coluna] = letra
-            posicao += 1
+            if letra in alfabeto:
+                alfabeto.remove(letra)
+                matriz[linha][coluna] = letra
+                posicao += 1
 
     while len(alfabeto) > 0:
         linha = int(posicao / 5)
@@ -113,8 +105,13 @@ def gerar_matriz_chave(chave: str):
 #################################################################
 #                         CRIPTOGRAFIA                          #
 #################################################################
-def cifrar(dupla, matriz):
-    pass
+def indice_do_elemento(matriz, elemento):
+    posicao = 0
+    for linha in matriz:
+        for coluna in linha:
+            if coluna == elemento:
+                return [int(posicao / 5), int(posicao % 5)]
+            posicao += 1
 
 
 def normalizar_texto(texto: str):
@@ -130,7 +127,8 @@ def normalizar_texto(texto: str):
 
             if separador < 2:
                 if i + 1 < tamanho_original and texto[i] == texto[i + 1]:
-                    novo_texto = novo_texto + "X"
+                    novo_texto = novo_texto + "X "
+                    separador = 0
 
                 separador += 1
             elif i + 1 < tamanho_original:
@@ -143,13 +141,55 @@ def normalizar_texto(texto: str):
     return novo_texto
 
 
+def concatena_caracter(primeira_posicao, proxima_posicao, matriz_chave, resultado):
+    if primeira_posicao[0] == proxima_posicao[0]:  # mesma linha
+        if (primeira_posicao[1] + 1) >= 5:  # se estourar a largura da matriz pega o da primeira posição da linha atual
+            resultado = resultado + matriz_chave[primeira_posicao[0]][0]
+        else:  # senão pega o próximo elemento da linha
+            resultado = resultado + matriz_chave[primeira_posicao[0]][primeira_posicao[1] + 1]
+    elif primeira_posicao[1] == proxima_posicao[1]:  # mesma primeira_posicao
+        if (primeira_posicao[0] + 1) >= 5:  # se estourar o tamanho da matriz pega o da primeira posição da coluna atual
+            resultado = resultado + matriz_chave[0][primeira_posicao[1]]
+        else:
+            resultado = resultado + matriz_chave[primeira_posicao[0] + 1][primeira_posicao[1]]
+    else:
+        resultado = resultado + matriz_chave[primeira_posicao[0]][proxima_posicao[1]]
+    return resultado
+
+
+def gerar_texto_criptografado(texto_normalizado, matriz_chave):
+    """
+    Mesma linha pega proxima a direita
+    Mesma coluna pega proxima linha abaixo
+    Senão avançar até a mesma coluna do outro par
+    :param texto_normalizado:
+    :param matriz_chave:
+    :return: texto cifrado
+    """
+    resultado = ""
+    tamanho_texto = len(texto_normalizado)
+    i = 0
+    while i < tamanho_texto:
+        atual = texto_normalizado[i]
+        proximo = texto_normalizado[i + 1]
+        posicao_atual = indice_do_elemento(matriz_chave, atual)
+        posicao_proximo = indice_do_elemento(matriz_chave, proximo)
+
+        resultado = concatena_caracter(posicao_atual, posicao_proximo, matriz_chave, resultado)  # concatena atual
+        resultado = concatena_caracter(posicao_proximo, posicao_atual, matriz_chave, resultado)  # concatena proximo
+
+        i += 3
+
+    return resultado
+
+
 def realizar_criptografia(conteudo, chave):
     print('================================================')
     print('Iniciando processamento')
-    print(normalizar_texto(conteudo))
-    print(gerar_matriz_chave(chave))
-    # matriz_chave = gerar_matriz_chave(chave)
-    return conteudo + chave
+    texto_normalizado = normalizar_texto(conteudo)
+    matriz_chave = gerar_matriz_chave(chave)
+
+    return gerar_texto_criptografado(texto_normalizado, matriz_chave)
 
 
 def cripografar():
